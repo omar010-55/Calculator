e1f51d78dfc49ae92adf91c009651b86e338c85c
omar mohamed <32615699+omar010-55@users.noreply.github.com>
2024-11-08 23:34:28 +0200

    Add number limits to not float
    Adding number limit to input and output

--- script.js
+++ script.js
@@ -18,17 +18,17 @@ let isOperatorClicked = false
 
 let isEqualRun = false 
 
-let firstNumber = " "
+let firstNumber = ""
 let operator = ""
-let secondNumber = " "
+let secondNumber = ""
 
 
 function reset() {
   isOperatorClicked = false
   isEqualRun = false 
-  firstNumber = " "
+  firstNumber = ""
   operator = ""
-  secondNumber = " "
+  secondNumber = ""
   refreshScreen(0)
 }
 
@@ -40,11 +40,12 @@ function operate(numOne, op , numTwo) { //Doing operations
   } else if(op === "*") {
     firstNumber = multiply(numOne,numTwo)
   } else {
-    firstNumber = divide(numOne,numTwo).toFixed(2)
+    firstNumber = divide(numOne,numTwo)
   }
+  firstNumber = firstNumber.toString().slice(0, 9)
   isOperatorClicked = false
   operator = ""
-  secondNumber = " "
+  secondNumber = ""
   refreshScreen(firstNumber)
 }
 // operate(1 , "+", 2)
@@ -56,23 +57,25 @@ function refreshScreen(pressed) { //Refreshing the screen and
 let allNumbers = document.querySelector(".allNumbers") 
 allNumbers.addEventListener("click", function(e) { //Sign the numbers
   if(e.target.className === "number") {
-    if(isOperatorClicked === true) {
+    if(isOperatorClicked === true && secondNumber.toString().length < 10) {
       secondNumber += `${e.target.id}`
       refreshScreen(secondNumber)
-    } else {
+    } else if(firstNumber.toString().length < 10) {
       if(isEqualRun === false) {
         firstNumber += `${e.target.id}`
+        refreshScreen(firstNumber)
       } else {
-        firstNumber = `${e.target.id}` 
+        firstNumber = `${e.target.id}`
+        refreshScreen(firstNumber)
       }
-      refreshScreen(firstNumber)
+      // refreshScreen(firstNumber)
     }
-  } else if(e.target.className === "operate" && isOperatorClicked === false && firstNumber !== " ") {
+  } else if(e.target.className === "operate" && isOperatorClicked === false && firstNumber !== "") {
     operator += `${e.target.id}`
     isOperatorClicked = true
     isEqualRun = false
   } else if(e.target.className === "equality") {
-    if(isOperatorClicked === true && secondNumber !== " ") {
+    if(isOperatorClicked === true && secondNumber !== "") {
       operate(firstNumber, operator, secondNumber)
       isEqualRun = true
     }
