bda484b38c592764e99b34af31b35e780cb8bd4d
omar mohamed <32615699+omar010-55@users.noreply.github.com>
2024-11-09 23:08:29 +0200

    Add the dot button and its safe logic
    add documenting to functions for more clarity and the dot button

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  if(+b === 0) {return "LoL"}
+  if(+b === 0) {return "LoL"}  //Divide by 0 safety
   return +a / +b
 }
 
@@ -24,7 +24,7 @@ let operator = ""
 let secondNumber = ""
 
 
-function reset() {
+function reset() {  //Reset all operations and variables
   isOperatorClicked = false
   isEqualRun = false 
   firstNumber = ""
@@ -33,7 +33,7 @@ function reset() {
   refreshScreen(0)
 }
 
-function operate(numOne, op , numTwo) { //Doing operations
+function operate(numOne, op , numTwo) {  //Doing operations based on op content
   if(op === "+") {
     firstNumber = add(numOne,numTwo)
   } else if(op === "-") {
@@ -43,41 +43,51 @@ function operate(numOne, op , numTwo) { //Doing operations
   } else {
     firstNumber = divide(numOne,numTwo)
   }
-  firstNumber = firstNumber.toString().slice(0, 9)
+  firstNumber = firstNumber.toString().slice(0, 9)  //Limit to 9 numbers
   isOperatorClicked = false
   operator = ""
   secondNumber = ""
   refreshScreen(firstNumber)
 }
-// operate(1 , "+", 2)
 let screen = document.querySelector("p")
-function refreshScreen(pressed) { //Refreshing the screen and
+function refreshScreen(pressed) {  //Refreshing the screen
   screen.textContent = `${pressed}` 
 }
 
 let allNumbers = document.querySelector(".allNumbers") 
-allNumbers.addEventListener("click", function(e) { //Sign the numbers
+allNumbers.addEventListener("click", function(e) { //Sign the numbers an all clicked buttons with checking the class and applying id
   if(e.target.className === "number") {
-    if(isOperatorClicked === true && secondNumber.toString().length < 9) {
-      secondNumber += `${e.target.id}`
+    if(isOperatorClicked === true && secondNumber.toString().length < 9) {  //To sign the second number based on condition
+      if(e.target.id === ".") {  //For the Dot logic its here to obey the previous roles of if
+        if(!secondNumber.split("").includes(".")) {  //To not applying two dots ..
+          secondNumber += `${e.target.id}`
+          return refreshScreen(secondNumber)
+        } else return //End and don't sign it 
+      }
+      secondNumber += `${e.target.id}` //If it's normal number not the dot "." then do the normal actions
       refreshScreen(secondNumber)
-    } else if(firstNumber.toString().length < 9) {
-      if(isEqualRun === false) {
+    } else if(firstNumber.toString().length < 9) {  //Limit to 9
+      if(isEqualRun === false) {  //If equal not clicked act normal
+        if(e.target.id === ".") {
+          if(!firstNumber.split("").includes(".")) {
+            firstNumber += `${e.target.id}`
+            return refreshScreen(firstNumber)
+          } else return
+        }
         firstNumber += `${e.target.id}`
         refreshScreen(firstNumber)
-      } else {
+      } else {  //If equal clicked reset firstNumber value after clicking numbers or dot again
         firstNumber = `${e.target.id}`
-        isEqualRun = false
+        isEqualRun = false  //Reset this logic
         refreshScreen(firstNumber)
       }
-      // refreshScreen(firstNumber)
     }
-  } else if(e.target.className === "operate" && isOperatorClicked === false && firstNumber !== "") {
+  } else if(e.target.className === "operate" && isOperatorClicked === false && firstNumber !== "") {  //First time to click operate
     operator += `${e.target.id}`
     isOperatorClicked = true
     isEqualRun = false
   } else if(e.target.className === "equality") {
-    if(isOperatorClicked === true && secondNumber !== "") {
+    if(isOperatorClicked === true && secondNumber !== "") {  //To not have Errors
       operate(firstNumber, operator, secondNumber)
       isEqualRun = true
     }
